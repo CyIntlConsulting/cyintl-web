7ecc905d7ef1d6a0e34657b593b0d81359cc92d0
Cleo <14207507+cleoisme@users.noreply.github.com>
2026-02-21 15:45:01 -0800

    remove cta - contact me form

--- themes/up-business-theme/assets/js/shared/shared.js
+++ themes/up-business-theme/assets/js/shared/shared.js
@@ -8,7 +8,6 @@ import "../sections/brands";
 import "../sections/portfolio";
 import "../sections/services";
 import "../sections/testimonials";
-import "../sections/cta";
 
 import "../sections/blog-list";
 import "../sections/blog-single";
