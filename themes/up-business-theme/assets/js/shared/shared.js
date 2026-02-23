31a9e047a1b87e66e45d7ed101eaae93c5b1acd4
Cleo <14207507+cleoisme@users.noreply.github.com>
2026-02-23 13:59:43 -0800

    add back cta

--- themes/up-business-theme/assets/js/shared/shared.js
+++ themes/up-business-theme/assets/js/shared/shared.js
@@ -8,6 +8,7 @@ import "../sections/brands";
 import "../sections/portfolio";
 import "../sections/services";
 import "../sections/testimonials";
+import "../sections/cta";
 
 import "../sections/blog-list";
 import "../sections/blog-single";
